136e1f9e7c3562fff03fce97f2a02d78724a6345
zWellingtonLima <0wellingtonlima1@gmail.com>
2022-11-20 21:46:38 +0000

    Pasta this adicionada para estudos em relação ao assunto.

---
+++ this/this.js
@@ -0,0 +1,15 @@
+// A palavra-chave this
+let user = {
+  name: 'Lima',
+  age: 25, 
+  email: 'lima@gmail.com',
+  city: 'Espinho',
+  blogPosts: ['Zé da coxinha', 'Do mil ao milhão em 30 minutos'],
+  login: function() {
+    console.log('Usuário logado');
+  },
+  logout: function() {
+    console.log('Usuário deslogado')
+  }
+}
+
