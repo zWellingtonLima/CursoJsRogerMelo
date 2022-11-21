d383678b66abfc34c32bd563a5cee15e35cbae50
zWellingtonLima <0wellingtonlima1@gmail.com>
2022-11-21 12:19:08 +0000

    Anotações iniciais sobre o uso do this.

--- this/this.js
+++ this/this.js
@@ -10,6 +10,24 @@ let user = {
   },
   logout: function() {
     console.log('Usuário deslogado')
+  },
+  logBlogPosts: function() {
+    console.log(`${this.name} escreveu os seguintes posts:`)
+
+    this.blogPosts.forEach(post => console.log(post))
+  },
+  hey() {
+    console.log("Olá")
   }
 }
 
+// Nota: A function Declaration é a que está sendo usada aqui e não a Arrow. Se eu quiser que o this referencie o objeto no qual o método foi criado é necessário usar Function Declaration.
+
+// A Arrow Function referencia o contexto global, no caso o Window Object, pois ela referencia no momento onde a invocação da  função é feita (linha 25 - contexto global.)
+
+user.logBlogPosts()
+user.hey()
+console.log(this) // Nesse contexto o Window é referenciado.
+
+// user.login()
+// user.logout()
