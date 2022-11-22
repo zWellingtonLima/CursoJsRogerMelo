0b699e2486e9700735d6e24e141412059853c9d8
zWellingtonLima <0wellingtonlima1@gmail.com>
2022-11-22 18:26:36 +0000

    Adição pasta DOM para exercícios de manipulação de elementos, classes, atributos etc.

---
+++ DOM/app.js
@@ -0,0 +1,37 @@
+// const paragraph = document.querySelector('p');
+
+// // paragraph.innerText += ' Texto inserido'
+
+// const paragraphs = document.querySelectorAll('p');
+
+// // paragraphs.forEach((paragraph, index) => {
+// //   paragraph.innerText += ` Novo texto ${index + 1}`
+// // })
+
+// const div = document.querySelector('.content');
+
+// // div.innerHTML = '<h2>Novo h2</h2>'
+// // div.innerHTML += '<h2>Novo h2</h2>'
+
+// const people = [ 'Jonatan', 'Vinicius', 'Diego']
+
+// people.forEach(person => {
+//   div.innerHTML += `<p>${person}</p>`
+// })
+
+// const paragraphs = document.querySelectorAll('p')
+
+// Incluir classes no elemento 'p' que contém os nomes error ou success
+
+// paragraphs.forEach(paragraph => {
+//   if (paragraph.innerText.includes('success')){
+//     paragraph.classList.add('success')
+//   } 
+//   if (paragraph.innerText.includes('error')){
+//     paragraph.classList.add('error')
+//   }
+// })
+
+// console.log(paragraphs);
+
+// Fim
