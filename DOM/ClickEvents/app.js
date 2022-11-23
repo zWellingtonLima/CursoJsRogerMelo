9047d019ab2410a3b6175b4434743b851340d44e
zWellingtonLima <0wellingtonlima1@gmail.com>
2022-11-23 21:22:34 +0000

    Modificações nos eventListeners

--- DOM/ClickEvents/app.js
+++ DOM/ClickEvents/app.js
@@ -1,16 +1,20 @@
 const button = document.querySelector('button');
+const ul = document.querySelector('ul')
 
 button.addEventListener('click', ()=> {
-
+  // ul.innerHTML += '<li>Novo item</li>' Essa é uma forma de criar um item a mais nessa ul.
+  const li = document.createElement('li')
+  li.innerText = 'Novo item'
+  ul.append(li) // Um problema que se dá aqui é que esses elementos criados não possuem um EventListener e por isso não são removidos quando clicados.
 })
 
-const ul = document.querySelector('ul')
 const lis = document.querySelectorAll('li')
 
 lis.forEach(li => {
   li.addEventListener('click', event => {
     const clickedElement = event.target
-
-    clickedElement.style.textDecoration = 'line-through'
+    
+    clickedElement.remove() 
   })
 })
+
