343bda8f1fe85c2349127647da1e8ff7b97e927f
zWellingtonLima <0wellingtonlima1@gmail.com>
2022-11-26 18:50:52 +0000

    Exercício refatorado e melhorado.

--- exercicio-16/app.js
+++ exercicio-16/app.js
@@ -7,21 +7,33 @@
 
 const div = document.querySelector('div')
 const elementsInsideDiv = Array.from(div.children)
-const h2 = document.querySelector('h2');
+const h2 = document.querySelector('h2')
+const circleDiv = document.querySelector('.egg')
 
-elementsInsideDiv.forEach(element => {
-  element.addEventListener('click', (event) => {
-    event.stopPropagation() // Basta usar o método stopPropagation do objeto event.
+// elementsInsideDiv.forEach(element => {
+//   element.addEventListener('click', (event) => {
+//     event.stopPropagation() // Basta usar o método stopPropagation do objeto event.
 
-      // console.log('Clicou no filho da div.')
-    console.log(`Clicou no <${event.target.tagName.toLowerCase()}>`)
-    h2.innerText = `Clicou no <${event.target.tagName.toLowerCase()}>`
-  })
-})
+//       // console.log('Clicou no filho da div.')
+//     console.log(`Clicou no <${event.target.tagName.toLowerCase()}>`)
+//     h2.innerText = `Clicou no <${event.target.tagName.toLowerCase()}>`
+//   })
+// }) // Não é necessário adicionar um listener para cada elemento da div para descobrir qual elemento foi clicado. É possível fazer isso na função abaixo.
 
-div.addEventListener('click', () => {
-  console.log('Clicou na div.')
-})
+const showClick = ( {target: { tagName }} ) => { // Aprofundamento de destructuring possibilita diminuir o encadeamento de propriedades (propriedades aninhadas, porém, caso eu precisar usar o valor da propriedade do nível anterior, no caso o target, o log dará um erro dizendo que target is not defined. Outra desvantagem é a de deixar o código um pouco menos legível.) Eu prefiro fazer o destructuring em apenas um nível.
+
+  const clickedElementTagName = tagName.toLowerCase
+
+  if (clickedElementTagName === 'div') {
+    h2.textContent = 'Clicou na div.'
+    return
+  }
+
+  h2.textContent = `Clicou no <${clickedElementTagName}>, filho da <div>.`
+
+} // Dentro da função eu checo se o elemento clicado é um filho da div ou a própria div.
+
+div.addEventListener('click', showClick)
 
 /*
   02
@@ -46,9 +58,9 @@ div.addEventListener('click', () => {
   - Faça com que quando o texto do h2 for copiado, a mensagem "Texto copiado!"  
     seja exibida no console.
 */
-h2.addEventListener('copy', () => {
-  console.log('h2 copiado!')
-})
+const logCopyMessage = () => console.log('h2 copiado!')
+
+h2.addEventListener('copy', logCopyMessage)
 /*
   05
 
@@ -57,11 +69,11 @@ h2.addEventListener('copy', () => {
     "Eixo X: COORDENADA_EIXO_X | Eixo Y: COORDENADA_EIXO_Y".
 */
 
-const circleDiv = document.querySelector('.egg');
+const showCoordinatesXAndYInsideEgg = ({ offsetX, offsetY}) => {
+  circleDiv.innerText = `Eixo X: ${offsetX} | Eixo Y: ${offsetY}`
+}
 
-circleDiv.addEventListener('mousemove', event => {
-  circleDiv.innerText = `X ${event.offsetX} | Y ${event.offsetY}`
-})
+circleDiv.addEventListener('mousemove', showCoordinatesXAndYInsideEgg)
 
 /*
   06
@@ -69,10 +81,11 @@ circleDiv.addEventListener('mousemove', event => {
   - Modifique a cor do ovo para "lightgoldenrodyellow" quando o botão for 
     clicado.
 */
-const button = document.querySelector('button');
-button.addEventListener('click', () => {
-  circleDiv.style.backgroundColor = 'lightgoldenrodyellow'
-})
+const button = document.querySelector('button')
+
+const changeEggColor = () => circleDiv.style.background = 'lightgoldenrodyellow'
+
+button.addEventListener('click', changeEggColor)
 
 /*
   07
@@ -90,13 +103,12 @@ const people = [
   { id: 4, name: 'Marcelo Antonio', profession: 'Matemático' },
   { id: 5, name: 'Camilla Midori', profession: 'Engenheira Civil' },
   { id: 6, name: 'Gustavo D\'Aqui', profession: 'Gerente de Marketing' },
+  { id: 7, name: 'Ana Paula', profession: 'Front-end developer' },
   { id: 8, name: 'Matheus Manucci', profession: 'Piloto' },
   { id: 9, name: 'Hamilton Silva', profession: 'Advogado' }
 ]
-const hasADeveloper = people.some(people => people.profession === 'Front-end developer')
+const hasADeveloper = people.some(({ profession }) => profession === 'Front-end developer')
 
 if (hasADeveloper) {
   console.log('O array contém no mínimo um desenvolvedor.')
-} else {
-  console.log('Não tem nenhum')
 }
