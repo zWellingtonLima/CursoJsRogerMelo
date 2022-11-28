f6bf578d7c4d81f6313a0b3019de42ec0c0a0a64
zWellingtonLima <0wellingtonlima1@gmail.com>
2022-11-28 17:18:51 +0000

    Código simples implementado para validação de formulário e envio de feedback sobre o valor inserido. O código atual permite fazer uma grande refatoração e implementar diversas melhorias.

--- 3 - FormEvents/app.js
+++ 3 - FormEvents/app.js
@@ -4,7 +4,25 @@ const usernameInput = document.querySelector('#username')
 form.addEventListener('submit', event => {
   event.preventDefault()
 
+  const input = event.target.username
+  const regexTest = /^[a-zA-z]{6,12}$/ 
+  const allowedValue = document.querySelector('.feedback');
+  const button = document.querySelector('button');
+  const p = document.createElement('p')
+
+    if (regexTest.test(input.value)) {
+      allowedValue.classList.add('allowed')
+      usernameInput.value = ''
+      usernameInput.focus()
+
+      button.appendChild(p)
+      p.innerText = 'Nome de usuário cadastrado com sucesso.'
+      return
+    }
+  
+    button.appendChild(p)
+    p.innerText = 'Nome de usuário não aceito. Tente novamente.'  
 })
 
-const username = 'roger';
-const reGex = /^[a-z]{6}$/
+// const username = 'roger';
+// const reGex = /^[a-z]{6}$/
