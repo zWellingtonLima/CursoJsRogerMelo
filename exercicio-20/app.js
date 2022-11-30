24d6a8fe9f681acf4dda6c9bc949adc37e7c1a24
zWellingtonLima <0wellingtonlima1@gmail.com>
2022-11-30 21:31:01 +0000

    Contador concluído.

--- exercicio-20/app.js
+++ exercicio-20/app.js
@@ -2,6 +2,7 @@
   01
 
   - Insira apenas o css do bootstrap no index.html.
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 */
 
 /* 
@@ -11,7 +12,9 @@
     mensagem "Um segundo e meio se passaram desde que a página foi carregada".
 */
 
-
+setTimeout(() => {
+  // console.log("Um segundo e meio se passaram desde que a página foi carregada.")
+}, 1500)
 
 /* 
   03
@@ -20,7 +23,31 @@
   - O clique no botão "Parar contador" deve fazer com que o contador exiba 0.
 */
 
+const buttonInit = document.querySelector('.button-init-counter');
+const buttonStop = document.querySelector('.button-stop-counter');
+const counter = document.querySelector('.counter-container');
 
+let interval
+const timer = () => {
+  interval = setInterval(() => {
+    counter.textContent++    
+  }, 1000)
+}
+
+buttonInit.addEventListener('click', () => {
+  timer()
+})
+
+buttonStop.addEventListener('click', () => {
+  clearInterval(interval)
+
+  setInterval(() => {
+    if (counter.textContent > 0) {
+      counter.textContent--
+    }
+  }, 100)
+
+})
 
 /* 
   04
