f85c40953ebc492cc26b61f470d2c2793de7a26e
zWellingtonLima <0wellingtonlima1@gmail.com>
2022-12-03 20:10:17 +0000

    Separação visual das diferentes funções que o callback do listener do form possui.

--- 0 - Aplicações/Quiz/app.js
+++ 0 - Aplicações/Quiz/app.js
@@ -3,11 +3,13 @@ const finalResult = document.querySelector('.result');
 
 const correctAnswers = ['B','B','B','B']
 
+// Função muito extensa. Refatorar para quebrar em funções isoladas.
 form.addEventListener('submit', event => {
   event.preventDefault()
 
   let score = 0
 
+  //Obtém as respostas do usuário.
   const userAnswers = [
     form.inputQuestion1.value,
     form.inputQuestion2.value,
@@ -15,16 +17,18 @@ form.addEventListener('submit', event => {
     form.inputQuestion4.value
   ]
 
+  // Calcular a pontuação do usuário.
   userAnswers.forEach((userAnswer,index) => {
     if (userAnswer === correctAnswers[index]){
       score += 25
     }
   })
 
+  // Exibir a pontuação final.
   scrollTo(0, 0)
-
   finalResult.classList.remove('d-none')
 
+  //Animação da pontuação final.
   let counter = 0
   const timer = setInterval(() => {
     if (counter === score) {
