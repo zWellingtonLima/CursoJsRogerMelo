327e1eef5ce081f03745152364f014ebd70ec5fd
zWellingtonLima <0wellingtonlima1@gmail.com>
2022-12-03 20:22:37 +0000

    Criadas funções isoladas que tratam dos diferentes pontos da aplicação.

--- 0 - Aplicações/Quiz/app.js
+++ 0 - Aplicações/Quiz/app.js
@@ -3,32 +3,32 @@ const finalResult = document.querySelector('.result');
 
 const correctAnswers = ['B','B','B','B']
 
-// Função muito extensa. Refatorar para quebrar em funções isoladas.
-form.addEventListener('submit', event => {
-  event.preventDefault()
+let score = 0
 
-  let score = 0
-
-  //Obtém as respostas do usuário.
+const getUserAnswers = () => {
   const userAnswers = [
-    form.inputQuestion1.value,
-    form.inputQuestion2.value,
-    form.inputQuestion3.value,
-    form.inputQuestion4.value
-  ]
+      form.inputQuestion1.value,
+      form.inputQuestion2.value,
+      form.inputQuestion3.value,
+      form.inputQuestion4.value
+    ]
+    return userAnswers
+}
 
-  // Calcular a pontuação do usuário.
+const calculateUserScore = userAnswers => {
   userAnswers.forEach((userAnswer,index) => {
     if (userAnswer === correctAnswers[index]){
       score += 25
     }
   })
+}
 
-  // Exibir a pontuação final.
+const showFinalScore = () => {
   scrollTo(0, 0)
   finalResult.classList.remove('d-none')
+}
 
-  //Animação da pontuação final.
+const animateFinalScore = () => {
   let counter = 0
   const timer = setInterval(() => {
     if (counter === score) {
@@ -38,6 +38,48 @@ form.addEventListener('submit', event => {
     finalResult.querySelector('span').textContent = `${counter}%`
     counter++
   }, 15)
+}
+
+// Função muito extensa. Refatorar para quebrar em funções isoladas.
+form.addEventListener('submit', event => {
+  event.preventDefault()
+
+  //Obtém as respostas do usuário.
+  // const userAnswers = [
+  //   form.inputQuestion1.value,
+  //   form.inputQuestion2.value,
+  //   form.inputQuestion3.value,
+  //   form.inputQuestion4.value
+  // ]
+  const userAnswers = getUserAnswers()
+
+  // Calcular a pontuação do usuário.
+  // userAnswers.forEach((userAnswer,index) => {
+  //   if (userAnswer === correctAnswers[index]){
+  //     score += 25
+  //   }
+  // })
+
+  calculateUserScore(userAnswers)
+
+  // Exibir a pontuação final.
+  // scrollTo(0, 0)
+  // finalResult.classList.remove('d-none')
+
+  showFinalScore()
+
+  //Animação da pontuação final.
+  // let counter = 0
+  // const timer = setInterval(() => {
+  //   if (counter === score) {
+  //     clearInterval(timer)
+  //   }
+
+  //   finalResult.querySelector('span').textContent = `${counter}%`
+  //   counter++
+  // }, 15)
+
+  animateFinalScore()
 
 })
 
