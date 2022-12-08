4596fa99973c712c3e3d69dcd557a9e42c9030e7
zWellingtonLima <0wellingtonlima1@gmail.com>
2022-12-08 10:34:15 +0000

    Exercício 22 concluído.

--- exercicio-22/app.js
+++ exercicio-22/app.js
@@ -7,6 +7,8 @@
 */
 
 const names = ['Caio', 'André', 'Dário']
+const namesCopy = names.map(name => name).sort()
+console.log(namesCopy, names)
 
 /*
   02
@@ -22,7 +24,11 @@ const characters = [
   { id: 01, name: 'Scar' },
   { id: 04, name: 'Mufasa' }
 ]
+const charactersCopy = characters.map(char => ({id: char.id, name: char.name})) // Esses parênteses envolvendo a criação do objeto são usados para que a abertura de chaves do objeto não seja confundido por abertura do bloco da função pela arrow da function. A expressão que tem maior precedência em JS é o que estiver dentro de parênteses, logo, o objeto vai ser criado primeiro e depois a arrow function atua retornando o objeto criado.
 
+charactersCopy.sort((item1, item2) => item1.id - item2.id)
+
+console.log(characters, charactersCopy) // Embora o map tenha gerado um novo array, como os itens do array são objetos (que são tipos de referência), os objetos do novo array não são uma cópia dos originais, eles apontam para o mesmo local. Se eu quiser fazer uma cópia é preciso eu retornar um novo objeto no map.
 /*
   03
 
@@ -32,14 +38,17 @@ const characters = [
 */
 
 const numbers = [41, 15, 63, 349, 25, 22, 143, 64, 59, 291]
+const numbersCopy = numbers.map(item => item).sort((item1, item2) => item1 - item2)
 
+console.log(numbers, numbersCopy)
 /*
   04
 
   - Encontre e exiba no console o 1º item maior que 50 do array abaixo.
 */
-
+// Usando o método find - ele retorna o primeiro item do array que corresponder à função de teste que passarmos. Se o find não encontrar um valor truthy ele retorna undefined.
 const randomNumbers = [10, 5, 0, 40, 60, 10, 20, 70]
+console.log(randomNumbers.find(item => item > 50))
 
 /*
   05
@@ -50,7 +59,9 @@ const randomNumbers = [10, 5, 0, 40, 60, 10, 20, 70]
 */
 
 const people = ['Cauã', 'Alfredo', 'Bruno']
+const peopleCopy = people.map(item => item).reverse()
 
+console.log(peopleCopy)
 /*
   06
   
@@ -60,7 +71,18 @@ const people = ['Cauã', 'Alfredo', 'Bruno']
 */
 
 const ingredients = ['vinho', 'tomate', 'cebola', 'cogumelo']
+const cookedIngredients = ingredients.reduce((acc, item, index) => {
+  const correctGenderLetter = item[item.length - 1] === 'a' ? 'cozida' : 'cozido' 
+  // Também posso fazer usando a ReGex /a$/.test(item)
+  
+  if (index === ingredients.length -1) {
+    return acc + `${item} ${correctGenderLetter}.`
+  }
 
+  return acc + `${item} ${correctGenderLetter}, `
+}, '')
+
+console.log(cookedIngredients)
 /*
   07
   
@@ -68,7 +90,7 @@ const ingredients = ['vinho', 'tomate', 'cebola', 'cogumelo']
     assistiram apenas os filmes da Disney.
 */
 
-const topBrazilmovies = [
+const topBrazilMovies = [
   { title: 'Vingadores: Ultimato', peopleAmount: 19686119, distributedBy: 'Disney' },
   { title: 'Titanic', peopleAmount: 17050000, distributedBy: 'Paramount / 20th Century' },
   { title: 'O Rei Leão', peopleAmount: 16267649, distributedBy: 'Disney' },
@@ -80,6 +102,16 @@ const topBrazilmovies = [
   { title: 'Os Vingadores', peopleAmount: 10968065, distributedBy: 'Disney' },
   { title: 'Dona Flor e Seus Dois Maridos', peopleAmount: 10735524, distributedBy: 'Embrafilme' }
 ]
+
+const onlyDisneyViwers = topBrazilMovies.reduce((acc, item) => {
+  if (item.distributedBy === 'Disney') {
+    acc += item.peopleAmount
+  }
+
+  return acc
+}, 0)
+
+console.log(onlyDisneyViwers + ' milhões de pessoas assistiram aos filmes da Disney.')
 
 /*
   08
@@ -101,12 +133,26 @@ const pets = [
   { name: 'Chico', age: 6, gender: 'Male', type: 'Dog' }
 ]
 
+const petsWithHumanAge = pets
+  .filter(({type}) => type === 'Dog')
+  .map(({name, age, type}) => ({name: name, HumanAge: age * 7, type: type
+}))
+
+console.log(petsWithHumanAge)
 /*
   09
   
-  - Considerando o array topBrazilmovies, através do map ou do reduce, insira 
+  - Considerando o array topBrazilMovies, através do map ou do reduce, insira 
     os nomes dos filmes na ul do index.html.
 */
+const ul = document.querySelector('.list-group');
+const li = document.createElement('li')
+
+const moviesList = topBrazilMovies.map(({title}) => `<li>${title}</li>`).join('')
+ul.innerHTML = moviesList
+console.log(moviesList)
+
+const movieNames = topBrazilMovies.reduce((acc, movie) => acc + `<li>${movie.title}</li>`, '')
 
 /*
   10
