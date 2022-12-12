812b560c6c8b113febf589961fded72483a51eb9
zWellingtonLima <0wellingtonlima1@gmail.com>
2022-12-12 09:29:03 +0000

    Adição facilitada dos toDos.

--- 0 - Aplicações/To-do/app.js
+++ 0 - Aplicações/To-do/app.js
@@ -1,40 +1,18 @@
-const formAddTodo = document.querySelector('.form-add-todo');
-const todosContainer = document.querySelector('.todos-container');
-const addInput = document.querySelector('[data-add="add"]')
+const formAddTodo = document.querySelector('.form-add-todo')
+const todosContainer = document.querySelector('.todos-container')
 
-formAddTodo.addEventListener('submit', event => {
-  event.preventDefault()
+formAddTodo.addEventListener('submit', e => {
+  e.preventDefault()
 
-  const inputValue = event.target.add.value.trim()
-  if (inputValue.length){
+  const inputValue = e.target.add.value.trim()
+  
+  if (inputValue.length) {
     todosContainer.innerHTML += `
-    <li class="list-group-item d-flex justify-content-between align-items-center">
-    <span>${inputValue}</span>
-    <i class="far fa-trash-alt delete"></i>
-    </li>`
+      <li class="list-group-item d-flex justify-content-between align-items-center">
+        <span>${inputValue}</span>
+        <i class="far fa-trash-alt delete"></i>
+      </li>
+    `
+    e.target.reset()
   }
 })
-
-
-// const addTodoValue = () => {
-//   const li = document.createElement('li')
-//   todosContainer.appendChild(li)
-//   li.innerHTML = `<span>${addInput.value}</span>`
-//   li.classList.add('list-group-item', 'd-flex', 'justify-content-between', 'align-items-center')
-
-//   return li
-// }
-
-// formAddTodo.addEventListener('submit', event => {
-//   event.preventDefault()
-
-//   const li = addTodoValue()
-
-//   const i = document.createElement('i');
-//   li.appendChild(i)
-
-//   i.classList.add('far', 'fa-trash-alt', 'delete')
-
-  
-//   addInput.value = ''
-// })
