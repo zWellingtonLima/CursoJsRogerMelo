06e11066ed4f079107dcdd0ee3752b112e4c9bc2
zWellingtonLima <0wellingtonlima1@gmail.com>
2022-12-15 18:02:58 +0000

    Funções isoladas dos Event Listeners

--- 0 - Aplicações/To-do/app.js
+++ 0 - Aplicações/To-do/app.js
@@ -2,28 +2,58 @@ const formAddTodo = document.querySelector('.form-add-todo')
 const todosContainer = document.querySelector('.todos-container')
 const inputSearchTodo = document.querySelector('.form-search input');
 
-formAddTodo.addEventListener('submit', e => {
-  e.preventDefault()
-
-  const inputValue = e.target.add.value.trim()
-  
+const addTodo = (inputValue, e) => {
   if (inputValue.length) {
     todosContainer.innerHTML += `
       <li class="list-group-item d-flex justify-content-between align-items-center" data-todo="${inputValue}">
         <span>${inputValue}</span>
-        <i class="far fa-trash-alt delete" data-trash="${inputValue}"></i>
+        <i class="far fa-trash-alt" data-trash="${inputValue}"></i>
       </li>
     `
     e.target.reset()
   }
+}
+
+formAddTodo.addEventListener('submit', e => {
+  e.preventDefault()
+
+  const inputValue = e.target.add.value.trim()
+  addTodo(inputValue, e)
+  // if (inputValue.length) {
+  //   todosContainer.innerHTML += `
+  //     <li class="list-group-item d-flex justify-content-between align-items-center" data-todo="${inputValue}">
+  //       <span>${inputValue}</span>
+  //       <i class="far fa-trash-alt" data-trash="${inputValue}"></i>
+  //     </li>
+  //   `
+  //   e.target.reset()
+  // }
 })
+
+const removeTodo = clickedElement => {
+  const trashDataValue = clickedElement.dataset.trash
+  const todo = document.querySelector(`[data-todo="${trashDataValue}"]`)
+
+  if (trashDataValue) {
+    todo.remove()
+  }
+}
 
 todosContainer.addEventListener('click', e => {
   const clickedElement = e.target
-  
-  if (clickedElement.dataset.trash) {
-    document.querySelector(`[data-todo="${clickedElement.dataset.trash}"]`).remove()
-  }
+  removeTodo(clickedElement)
+
+  // ------------------------- O código abaixo tem a função de rmover o elemento se a lixeira dele for clicada. 
+  // const trashDataValue = clickedElement.dataset.trash
+  // const todo = document.querySelector(`[data-todo="${trashDataValue}"]`)
+
+  // if (trashDataValue) {
+  //   todo.remove()
+  // }
+  // --------------------------------------
+
+  // Código abaixo alterado para uma solução que não envolva navegar pelo dom para encontrar elementos pois o código pode quebrar se a marcação mudar.
+
   // const removeIconClicked = Array.from(clickedElement.classList).includes('delete')
 
   // if (removeIconClicked){
@@ -31,19 +61,43 @@ todosContainer.addEventListener('click', e => {
   // }
 })
 
-inputSearchTodo.addEventListener('input', e => {
-  e.preventDefault() 
-
-  Array.from(todosContainer.children)
+const hideTodos = (inputValue, todos) => {
+  todos
     .filter(toDo => !toDo.textContent.toLowerCase().includes(inputValue))
     .forEach(toDo => {
       toDo.classList.remove('d-flex') 
       toDo.classList.add('hidden')
-    })
-  Array.from(todosContainer.children)
-  .filter(toDo => toDo.textContent.toLowerCase().includes(inputValue))
-  .forEach(toDo => {
-    toDo.classList.remove('hidden') 
-    toDo.classList.add('d-flex')
   })
+}
+
+const showTodos = (inputValue, todos) => {
+  todos
+    .filter(toDo => toDo.textContent.toLowerCase().includes(inputValue))
+    .forEach(toDo => {
+      toDo.classList.remove('hidden') 
+      toDo.classList.add('d-flex')
+  })
+}
+
+inputSearchTodo.addEventListener('input', e => {
+  const inputValue = e.target.value.trim().toLowerCase()
+  const todos = Array.from(todosContainer.children)
+
+  hideTodos(inputValue, todos)
+
+  // todos
+  //   .filter(toDo => !toDo.textContent.toLowerCase().includes(inputValue))
+  //   .forEach(toDo => {
+  //     toDo.classList.remove('d-flex') 
+  //     toDo.classList.add('hidden')
+  //   })
+
+  showTodos(inputValue, todos)
+
+// todos
+//   .filter(toDo => toDo.textContent.toLowerCase().includes(inputValue))
+//   .forEach(toDo => {
+//     toDo.classList.remove('hidden') 
+//     toDo.classList.add('d-flex')
+//   })
 })
