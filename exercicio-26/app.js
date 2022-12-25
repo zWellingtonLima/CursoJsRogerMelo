060755ad23725acddc498a62a5d16b7e5900718c
zWellingtonLima <0wellingtonlima1@gmail.com>
2022-12-25 20:52:37 +0000

    Função getClockHTML criada para lidar isoladamente com a criação do template que gera as horas.

--- exercicio-26/app.js
+++ exercicio-26/app.js
@@ -107,17 +107,19 @@ updateSomething({ target: '1', property: '2', willChange: 'valor indesejado' })
 
 const clockContainer = document.querySelector('.clock-container')
 
+const getClockHTML = (hours, minutes, seconds) => `
+    <span>${formatTimeUnit(hours)}</span> :
+    <span>${formatTimeUnit(minutes)}</span> :
+    <span>${formatTimeUnit(seconds)}</span>
+  `
+
 const updateClock = () => {
   const present = new Date()
   const hours = present.getHours()
   const minutes = present.getMinutes()
   const seconds = present.getSeconds()
 
-  clockContainer.innerHTML = `
-    <span>${formatTimeUnit(hours)}</span> :
-    <span>${formatTimeUnit(minutes)}</span> :
-    <span>${formatTimeUnit(seconds)}</span>
-  `
+  clockContainer.innerHTML = getClockHTML(hours, minutes, seconds)
 }
 
 setInterval(updateClock, 1000)
