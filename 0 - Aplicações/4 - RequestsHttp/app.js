74b6acf8b4c914dd6d39555bb790dbeb2301dd19
zWellingtonLima <0wellingtonlima1@gmail.com>
2023-01-01 20:44:44 +0000

    Função getTodos criada para ter todo o código do request com a possibilidade de ser reaproveitado.

--- 0 - Aplicações/4 - RequestsHttp/app.js
+++ 0 - Aplicações/4 - RequestsHttp/app.js
@@ -1,15 +1,20 @@
-const request = new XMLHttpRequest()
+const getTodos = () => {
+  const request = new XMLHttpRequest()
 
-request.addEventListener('readystatechange', () => {
-  const sucessfullRequest = request.readyState === 4 && request.status === 200
+  request.addEventListener('readystatechange', () => {
+    const sucessfullRequest = request.readyState === 4 && request.status === 200
 
-  if (sucessfullRequest) {
-    return console.log(request.responseText)
-  }
+    if (sucessfullRequest) {
+      return console.log(request.responseText)
+    }
 
-  return 'Não foi possível obter os dados da requisição.'
-})
+    if (request.readyState === 4) {
+      return console.log('Não foi possível obter os dados da requisição.')
+    }
+  })
 
-request.open('GET', 'https://jsonplaceholder.typicode.com/todos')
-request.send()
+  request.open('GET', 'https://jsonplaceholder.typicode.com/todos')
+  request.send()
+}
 
+getTodos()
