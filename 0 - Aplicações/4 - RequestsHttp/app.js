df49eceb5930b765b4a9f35b8d6c2bd7a0f08238
zWellingtonLima <0wellingtonlima1@gmail.com>
2023-01-01 20:38:25 +0000

    const sucessfullRequest criada para melhorar a legibilidade da condição que existe no eventListener.

--- 0 - Aplicações/4 - RequestsHttp/app.js
+++ 0 - Aplicações/4 - RequestsHttp/app.js
@@ -1,12 +1,15 @@
 const request = new XMLHttpRequest()
 
 request.addEventListener('readystatechange', () => {
-  if (request.readyState === 4 && request.status === 200) {
-    console.log(request.responseText)
+  const sucessfullRequest = request.readyState === 4 && request.status === 200
+
+  if (sucessfullRequest) {
+    return console.log(request.responseText)
   }
+
+  return 'Não foi possível obter os dados da requisição.'
 })
 
 request.open('GET', 'https://jsonplaceholder.typicode.com/todos')
 request.send()
 
-console.log(request)
